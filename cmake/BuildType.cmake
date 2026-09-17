ad5524bb370975961d6be0318206f59d6fe05f1c
Matt Wilson <matthew@synesis.com.au>
2026-09-18 06:44:39 +1000

    chore(CMake files): SPACEs => TABs

--- cmake/BuildType.cmake
+++ cmake/BuildType.cmake
@@ -5,7 +5,7 @@
 # Purpose:  CMake module file (for BuildType)
 #
 # Created:  16th October 2019
-# Updated:  12th January 2024
+# Updated:  18th September 2026
 #
 # ######################################################################## #
 
@@ -37,24 +37,24 @@ include(BuildType)
 
 if(EXISTS "${CMAKE_SOURCE_DIR}/.git")
 
-    set(default_build_type "Debug")
+	set(default_build_type "Debug")
 else()
 
-    set(default_build_type "Release")
+	set(default_build_type "Release")
 endif()
 
 if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
 
-    message(STATUS "Setting build type to '${default_build_type}' as none was specified.")
+	message(STATUS "Setting build type to '${default_build_type}' as none was specified.")
 
-    set(CACHE CMAKE_BUILD_TYPE "${default_build_type}"
-        STRING "Choose the type of build." FORCE
-    )
+	set(CACHE CMAKE_BUILD_TYPE "${default_build_type}"
+		STRING "Choose the type of build." FORCE
+	)
 
-    # Set the possible values of build type for cmake-gui
-    set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS
-        "Debug" "Release" "MinSizeRel" "RelWithDebInfo"
-    )
+	# Set the possible values of build type for cmake-gui
+	set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS
+		"Debug" "Release" "MinSizeRel" "RelWithDebInfo"
+	)
 endif()
 
 
